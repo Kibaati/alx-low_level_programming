0c6c237875d3cf1bf7240debb7b29df5f99d32ee
kibaati <amoskibaati25@gmail.com>
2023-08-10 17:47:16 -0700

    Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - main function
+ *Return:0 always
+ */
+int main(void)
+{
+
+       printf("Size of a char:%lu byte(s)", sizeof(char))
+       printf("size of int: %lu byte(s)", sizeof(int))
+       printf("size of long int: %lu byte(s)", sizeof()) 
+}
