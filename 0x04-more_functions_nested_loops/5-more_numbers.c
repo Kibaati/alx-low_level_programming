93983690616a717d9cadf25843c37834684cde7c
Kibaati <amoskibaati25@gmail.com>
2023-08-17 09:33:48 -0700

    Numbers constitute

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * more_numbers - print 10 times more
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+int i;
+int j;
+
+for (i = 0 ; i < 10 ; i++)
+{
+for (j = 0 ; j < 15 ; j++)
+{
+putchar(j + '0');
+}
+putchar('\n');
+}
+putchar('\n');
+}
