d6ec0260b793a04696a6adeb181fe3b70c59b7cf
Kibaati <amoskibaati25@gmail.com>
2023-08-28 06:14:04 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+* _strpbrk - locates character in a string
+* @s: string to search
+* @accept: character to search in s
+* Return: accept or NULL
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+while (*s != '\0')
+{
+const char *a = accept;
+
+while (*a != '\0')
+{
+if (*s == *a)
+return (s);
+a++;
+}
+s++;
+}
+
+return (NULL);
+}
