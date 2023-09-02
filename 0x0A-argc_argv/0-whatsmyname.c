486ddbe76fa4f29fec8eed3e37980e177625ba96
Kibaati <amoskibaati25@gmail.com>
2023-09-01 17:30:43 -0700

    It ain't what they call you

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,10 @@
  * @argv: argument vector
  * Return: 0
  */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
+(void) agrc;
+
 printf("%s\n", argv[0]);
 return (0);
 }
